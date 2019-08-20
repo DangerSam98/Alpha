fee2a264ec0d24d692e7d31401a4c2dda1d1da27
root <root@ip-172-31-45-185.us-east-2.compute.internal>
2019-08-20 10:21:22 +0000

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+	""""Add Function"""
+		return x+y 
+
+def subtract(x,y):
+	"""Subtract Function"""
+		return x-y 
+
+def multiply
+	"""multiply Function"""
+	return x * y
+
+def divide(x,y)
+	"""Divivde Function"""
+	return x / y
